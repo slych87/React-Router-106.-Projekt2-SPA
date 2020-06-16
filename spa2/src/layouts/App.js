cd2e2803f1d06e3b2d9be57a2d86243899bd878d
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-16 14:59:19 +0200

    work on the layout

--- spa2/src/layouts/App.js
+++ spa2/src/layouts/App.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 
 import '../styles/App.css';
 
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        SPA
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            {<Header />}
+          </header>
+          <main>
+            <aside>
+              {<Navigation />}
+            </aside>
+            <section className="page">
+              {<Page />}
+            </section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
     );
   }
 }
