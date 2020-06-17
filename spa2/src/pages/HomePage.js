334f8dbbb122fbab58d2013b234e1e887844833d
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-17 20:16:07 +0200

    contact tab and protection against loss data on the form

--- spa2/src/pages/HomePage.js
+++ spa2/src/pages/HomePage.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import Article from "../components/Article"
 import { Route } from 'react-router-dom';
 
+const articles = [
+    {
+        id: 1,
+        title: "Czym jest teoria strun",
+        author: "Jan Nowak",
+        text: "1111Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque amet, vitae consequatur exercitationem ratione voluptatibus repudiandae alias deleniti culpa illo suscipit quia sint temporibus illum commodi itaque ea ab ipsum?"
+    },
+    {
+        id: 2,
+        title: "Czym jest paradoks fermiego?",
+        author: "Ania Kwiatkowska",
+        text: "2222Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cumque accusantium sapiente perspiciatis id ipsam necessitatibus recusandae unde dolores error sunt officiis ad, voluptatum veniam, blanditiis nam in totam esse."
+    },
+    {
+        id: 3,
+        title: "Ciemna materia i ciemna energia?",
+        author: "Jan Kowalski",
+        text: "4444Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi cumque accusantium sapiente perspiciatis id ipsam necessitatibus recusandae unde dolores error sunt officiis ad, voluptatum veniam, blanditiis nam in totam esse."
+    }
+]
+
 const HomePage = () => {
+
+    const artList = articles.map(article => (
+        <Article key={article.id} {...article} />
+    ))
+
     return (
         <>
-            <div>HomePage</div>
+            <div className="home">
+                {artList}
+            </div>
         </>
     );
 }
