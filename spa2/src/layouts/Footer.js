9a85a7d6cec0257a190128636f70eeb14919c301
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-06-18 15:43:56 +0200

    project done

--- spa2/src/layouts/Footer.js
+++ spa2/src/layouts/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
         <div>
-            Stopka
+            <h2>Stopka</h2>
+            <Route path="/" exact render={(props) => (
+                <p>Jesteś na <span>stronie głównej</span></p>
+            )
+            } />
+
+            <Route path="/:page" exact render={(props) => {
+                console.log(props)
+                return (
+                    <p>Jesteś na <span>{props.match.params.page}</span></p>
+                )
+            }} />
+            <Route path="/:page/:idProduct" exact render={(props) => {
+                console.log(props)
+                return (
+                    <p>Jesteś na <span>{props.match.params.idProduct}</span></p>
+                )
+            }} />
+
         </div>
     );
 }
 
-export default Footer;
+export default Footer; 
